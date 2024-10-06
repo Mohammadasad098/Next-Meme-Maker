5506e15c91a18f4bf09992b27a44bf966e39eb4a
Mohammadasad098 <mohammadasadiam64@gmail.com>
2024-10-07 02:04:27 +0500

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "i.imgflip.com",
+          port: "",
+          search: "",
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
